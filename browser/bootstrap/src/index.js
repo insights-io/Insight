faf6ae6221cdf969a60c041fc6c2e5fc133ca556
Matej Šnuderl <matej.snuderl@cognite.com>
2020-02-02 17:32:20 +0100

    build step (#10)

--- browser/bootstrap/src/index.js
+++ browser/bootstrap/src/index.js
@@ -1,16 +1,17 @@
+/* eslint-disable no-param-reassign */
 /* eslint-disable @typescript-eslint/camelcase */
 /* eslint-disable no-underscore-dangle */
-window._i_debug = !1;
-window._i_host = 'insight.com';
-window._i_script = 'cdn.insight.com/s/is.js';
-window._i_org = '<ORG>';
-window._i_ns = 'IS';
 
-((doc, scriptTag) => {
+((window, doc, scriptTag) => {
+  window._i_debug = !1;
+  window._i_host = 'insight.com';
+  window._i_org = '<ORG>';
+  window._i_ns = 'IS';
+
   const scriptElement = doc.createElement(scriptTag);
   scriptElement.async = true;
   scriptElement.crossOrigin = 'anonymous';
-  scriptElement.src = `https://${window._i_script}`;
+  scriptElement.src = 'https://d2c0kshu2rj5p.cloudfront.net/s/insight.js';
   const firstScript = doc.getElementsByTagName(scriptTag)[0];
   firstScript.parentNode.insertBefore(scriptElement, firstScript);
-})(document, 'script');
+})(window, document, 'script');
--- browser/tracking/rollup.config.js
+++ browser/tracking/rollup.config.js
@@ -1,13 +1,18 @@
 /* eslint-disable @typescript-eslint/camelcase */
 import { terser } from 'rollup-plugin-terser';
 import typescript from '@rollup/plugin-typescript';
+import gzipPlugin from 'rollup-plugin-gzip';
 
 const input = 'src/index.ts';
 
 export default {
   input,
-  plugins: [typescript(), terser({ compress: true, mangle: true })],
+  plugins: [
+    typescript(),
+    terser({ compress: true, mangle: true }),
+    gzipPlugin(),
+  ],
   output: {
-    file: 'dist/index.js',
+    file: 'dist/insight.js',
   },
 };
