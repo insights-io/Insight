5c8805b41692d2e5371d95b9838d00cca262a6aa
Matej Šnuderl <ematej.snuderl@gmail.com>
2020-05-12 22:14:02 +0200

    chore: bump flyway (#60)

--- backend/auth/auth-api/migrations/scripts/flyway.sh
+++ backend/auth/auth-api/migrations/scripts/flyway.sh
@@ -7,6 +7,6 @@ POSTGRES_PASSWORD=${POSTGRES_PASSWORD:="postgres"}
 
 JDBC_URL="jdbc:postgresql://${POSTGRES_HOST}/${POSTGRES_DB}"
 
-echo "Flyway connecting to ${JDBC_URL} via user ${DATABASE_USER}" 
+echo "Flyway connecting to ${JDBC_URL} via user ${DATABASE_USER}"
 
-/flyway/flyway -url=${JDBC_URL} -user=${POSTGRES_USER} -password=${POSTGRES_PASSWORD} -schemas=auth -locations=filesystem:sql -connectRetries=60 $@
+/flyway/flyway -url=${JDBC_URL} -user=${POSTGRES_USER} -password=${POSTGRES_PASSWORD} -locations=filesystem:sql -connectRetries=60 "$@"
