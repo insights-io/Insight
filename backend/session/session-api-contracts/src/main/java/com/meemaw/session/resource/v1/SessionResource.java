edd458be1d37a38560fee4a4675e99ab857917b1
Matej Šnuderl <ematej.snuderl@gmail.com>
2020-05-13 10:30:50 +0200

    chore: split session-api (#64)
    * chore: split session-api
    
    * chore: fix test name

--- backend/session/session-api-contracts/src/main/java/com/meemaw/session/resource/v1/SessionResource.java
+++ backend/session/session-api-contracts/src/main/java/com/meemaw/session/resource/v1/SessionResource.java
@@ -16,10 +16,12 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
 
 @Path(SessionResource.PATH)
 @Consumes(MediaType.APPLICATION_JSON)
 @Produces(MediaType.APPLICATION_JSON)
+@RegisterRestClient(configKey = "session-resource")
 public interface SessionResource {
 
   String PATH = "/v1/sessions";
