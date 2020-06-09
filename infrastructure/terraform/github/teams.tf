c522e925507897ad329216c8da3d4d2f0bf1da60
Matej Snuderl <ematej.snuderl@gmail.com>
2020-06-09 14:29:17 +0200

    infra team

---
+++ infrastructure/terraform/github/teams.tf
@@ -0,0 +1,14 @@
+locals {
+  infrastructure_maintainers = ["Meemaw", "snuderl"]
+}
+
+resource "github_team" "infrastructure" {
+  name = "infrastructure"
+}
+
+resource "github_team_membership" "infrastructure_maintainers" {
+  for_each = toset(local.infrastructure_members)
+  team_id  = github_team.infrastructure.id
+  username = each.value
+  role     = "maintainer"
+}
