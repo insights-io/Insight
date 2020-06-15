25cdd190bd90ff02b84216480327fe3c76f9f88d
Matej Šnuderl <ematej.snuderl@gmail.com>
2020-06-15 21:22:49 +0200

    Fix try base url-v2 (#73)
    * fix: try base url
    
    * console log env
    
    * cleanup

--- frontend/app/next.config.js
+++ frontend/app/next.config.js
@@ -15,15 +15,19 @@ const {
   NEXT_PUBLIC_SESSION_API_BASE_URL = 'http://localhost:8082',
 } = process.env;
 
+const env = {
+  NEXT_PUBLIC_TRY_BASE_URL,
+  AUTH_API_BASE_URL,
+  NEXT_PUBLIC_AUTH_API_BASE_URL,
+  SESSION_API_BASE_URL,
+  NEXT_PUBLIC_SESSION_API_BASE_URL,
+};
+
+console.log('App environment:', env);
+
 module.exports = withServiceProxy(
   withBundleAnalyzer({
-    env: {
-      NEXT_PUBLIC_TRY_BASE_URL,
-      AUTH_API_BASE_URL,
-      NEXT_PUBLIC_AUTH_API_BASE_URL,
-      SESSION_API_BASE_URL,
-      NEXT_PUBLIC_SESSION_API_BASE_URL,
-    },
+    env,
     webpack: (config, _config) => config,
     experimental: { optionalCatchAll: true },
   })
--- frontend/try/next.config.js
+++ frontend/try/next.config.js
@@ -10,18 +10,20 @@ const withServiceProxy = require('@insight/service-proxy').default({
 const {
   APP_BASE_URL = 'http://localhost:3000',
   HELP_BASE_URL = 'http://localhost:3003',
-  AUTH_API_BASE_URL = 'http://localhost:8080',
   NEXT_PUBLIC_AUTH_API_BASE_URL = 'http://localhost:8080',
 } = process.env;
 
+const env = {
+  APP_BASE_URL,
+  HELP_BASE_URL,
+  NEXT_PUBLIC_AUTH_API_BASE_URL,
+};
+
+console.log('Try environment:', env);
+
 module.exports = withServiceProxy(
   withBundleAnalyzer({
-    env: {
-      APP_BASE_URL,
-      HELP_BASE_URL,
-      AUTH_API_BASE_URL,
-      NEXT_PUBLIC_AUTH_API_BASE_URL,
-    },
+    env,
     webpack: (config, _config) => config,
     experimental: { optionalCatchAll: true },
   })
