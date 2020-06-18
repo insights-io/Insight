fee13f469a44d32e46ab770322b511cd27dfee45
Matej Snuderl <ematej.snuderl@gmail.com>
2020-06-18 16:11:02 +0200

    small cleanup

--- backend/shared/rest/src/main/java/com/meemaw/shared/rest/FilterExpression.java
+++ backend/shared/rest/src/main/java/com/meemaw/shared/rest/FilterExpression.java
@@ -1,14 +1,9 @@
 package com.meemaw.shared.rest;
 
-import java.util.List;
-import lombok.Data;
 import org.jooq.SelectConditionStep;
 import org.jooq.SelectJoinStep;
 
-@Data
 public abstract class FilterExpression {
-
-  List<FilterExpression> children;
 
   /**
    * Update query with filter expression.
