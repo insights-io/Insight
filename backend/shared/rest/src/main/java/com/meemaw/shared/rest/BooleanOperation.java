7f2c28fe8e8e560a6268757758ee33377bfc673a
Matej Snuderl <ematej.snuderl@gmail.com>
2020-06-18 16:38:46 +0200

    final cleanup

--- backend/shared/rest/src/main/java/com/meemaw/shared/rest/BooleanOperation.java
+++ backend/shared/rest/src/main/java/com/meemaw/shared/rest/BooleanOperation.java
@@ -1,32 +1,25 @@
 package com.meemaw.shared.rest;
 
-import lombok.Getter;
 import lombok.ToString;
 import org.jooq.Condition;
 import org.jooq.SelectConditionStep;
 
 @ToString
 public enum BooleanOperation {
-  AND("AND") {
+  AND {
     @Override
     public SelectConditionStep<?> applyCondition(
         SelectConditionStep<?> field, Condition condition) {
       return field.and(condition);
     }
   },
-  OR("OR") {
+  OR {
     @Override
     public SelectConditionStep<?> applyCondition(
         SelectConditionStep<?> field, Condition condition) {
       return field.or(condition);
     }
   };
-
-  @Getter private final String condition;
-
-  BooleanOperation(String condition) {
-    this.condition = condition;
-  }
 
   public abstract SelectConditionStep<?> applyCondition(
       SelectConditionStep<?> query, Condition condition);
