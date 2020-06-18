596839c8a946df55671c9fadb1ccfb4c28502343
Matej Snuderl <ematej.snuderl@gmail.com>
2020-06-18 09:16:54 +0200

    fixes

--- infrastructure/terraform/staging/browser/main.tf
+++ infrastructure/terraform/staging/browser/main.tf
@@ -3,7 +3,6 @@ module "browser" {
   bucket_name   = "insight-${module.project_vars.environment}-static"
   environment   = module.project_vars.environment
   region        = module.project_vars.region
-  zone_id       = "5c1425c7c9c3dac30330217af5b3349c"
   alias         = "static.dev.snuderls.eu"
   acme_email    = "blaz.snuderl@gmail.com"
 }
--- infrastructure/terraform/staging/dns/main.tf
+++ infrastructure/terraform/staging/dns/main.tf
@@ -1,7 +1,7 @@
 # Point * to Kubernetes server
 resource "cloudflare_record" "wildcard" {
-  zone_id = var.zone_id
-  name    = "*${var.domain}"
+  zone_id = "5c1425c7c9c3dac30330217af5b3349c"
+  name    = "*dev.snuderls"
   value   = "213.161.29.246"
   type    = "A"
   ttl     = 1
