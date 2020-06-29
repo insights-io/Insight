d26880c628bd0d0ab4c28d3b0ab9945caaf9e18a
Matej Snuderl <ematej.snuderl@gmail.com>
2020-06-29 13:55:09 +0200

    fixes 2

--- backend/session/session-api/migrations/elasticsearch/scripts/main.go
+++ backend/session/session-api/migrations/elasticsearch/scripts/main.go
@@ -38,18 +38,30 @@ func createElasticsearchClient() *elasticsearch.Client {
 	return client
 }
 
-func parseMapping(content []byte) (string, string) {
+func parseMigration(content []byte) (string, map[string]json.RawMessage) {
 	lines := strings.Split(string(content), "\n")
 	index := strings.Split(lines[0], " ")[1][1:]
-	mappings := strings.Join(lines[1:], "\n")
+	body := strings.Join(lines[1:], "\n")
 
-	var mappingsMap map[string]json.RawMessage
-	err := json.Unmarshal([]byte(mappings), &mappingsMap)
+	var migration map[string]json.RawMessage
+	err := json.Unmarshal([]byte(body), &migration)
 	if err != nil {
-		log.Fatalf("Failed to parse mappings: %s", mappings)
+		log.Fatalf("Failed to migration body: %s", body)
 	}
 
-	return index, string(mappingsMap["mappings"])
+	return index, migration
+}
+
+func indexExists(es *elasticsearch.Client, ctx context.Context, index string) bool {
+	res, err := esapi.IndicesExistsRequest{Index: []string{index}}.Do(ctx, es)
+	if err != nil {
+		log.Fatalf("Error getting response: %s", err)
+	}
+	defer res.Body.Close()
+	if res.IsError() && res.StatusCode != 404 {
+		log.Fatalf("[%d] Error checking if index exists", res.StatusCode)
+	}
+	return res.StatusCode == 200
 }
 
 func main() {
@@ -58,34 +70,41 @@ func main() {
 	path, _ := os.Getwd()
 	mappingsPath := strings.Join([]string{path, "..", "mappings"}, "/")
 	migrationFiles := readMigrationFiles(mappingsPath)
-	log.Printf("Reading mappings from path=%s", mappingsPath)
+	log.Printf("Searching for migrations in: %s", mappingsPath)
 
 	for _, file := range migrationFiles {
 		filePath := strings.Join([]string{mappingsPath, file.Name()}, "/")
+		log.Printf("Applying migration from: %s", filePath)
+
 		content, _ := ioutil.ReadFile(filePath)
-		index, mappings := parseMapping(content)
+		index, migration := parseMigration(content)
 
-		res, err := esapi.IndicesCreateRequest{
-			Index: index,
-		}.Do(ctx, es)
-
-		if err != nil {
-			log.Fatalf("Error getting response: %s", err)
-		}
-		defer res.Body.Close()
-		if res.IsError() {
-			log.Printf("[%s] Error creating index=%s path=%s", res.Status(), index, filePath)
-		} else {
-			log.Printf("Put mappings [%s]: %s", index, mappings)
-
-			res, err = esapi.IndicesPutMappingRequest{
-				Index: []string{index},
-				Body:  strings.NewReader(mappings),
-			}.Do(ctx, es)
-
+		if !indexExists(es, ctx, index) {
+			log.Printf("Creating index: %s", index)
+			res, err := esapi.IndicesCreateRequest{Index: index}.Do(ctx, es)
 			if err != nil {
 				log.Fatalf("Error getting response: %s", err)
 			}
+			defer res.Body.Close()
+			if res.IsError() {
+				log.Printf("[%s] Error creating index=%s path=%s", res.Status(), index, filePath)
+				continue
+			}
+		}
+
+		if len(migration["mappings"]) > 0 {
+			mappings := string(migration["mappings"])
+			log.Printf("Creating mappings for index: %s %s", index, mappings)
+
+			res, err := esapi.IndicesPutMappingRequest{Index: []string{index}, Body: strings.NewReader(mappings)}.Do(ctx, es)
+			if err != nil {
+				log.Fatalf("Error getting response: %s", err)
+			}
+			defer res.Body.Close()
+			if res.IsError() {
+				log.Printf("[%d] Error creating mapping for index=%s", res.StatusCode, index)
+			}
+
 		}
 	}
 
