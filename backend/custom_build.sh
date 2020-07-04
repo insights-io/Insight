afc5bafbbee81461a5fd3d6e7b8032a4e4ae854e
Blaž Šnuderl <snuderl@gmail.com>
2020-07-04 16:47:19 +0200

    skaffold

---
+++ backend/custom_build.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+set -ex
+args=()  
+args+=('--tag' "$IMAGE")
+args+=('--builder' 'qemu')
+args+=('--network' 'host')
+if [ "$PUSH_IMAGE" = "true" ];
+then
+  args+=('--platform' 'linux/arm/v7,linux/amd64,linux/arm64')
+  args+=('-o' 'type=registry')
+else
+  args+=('--platform' 'linux/amd64')
+  args+=('-o' 'type=docker,dest=out')
+fi
+
+args+=('-f' "$1" "$BUILD_CONTEXT")
+docker buildx build "${args[@]}"
+
+if [ "$PUSH_IMAGE" = "false" ];
+then
+  docker load -i out
+  rm out
+fi
