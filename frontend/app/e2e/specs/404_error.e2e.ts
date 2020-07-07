32ae4ef989a9532131996d73c084f7cf01a9615a
Matej Snuderl <ematej.snuderl@gmail.com>
2020-07-07 21:05:52 +0200

    404_error test name

--- frontend/app/e2e/specs/404_error.e2e.ts
+++ frontend/app/e2e/specs/404_error.e2e.ts
@@ -1,8 +1,8 @@
 import config from '../config';
 import { getLocation } from '../utils';
 
-fixture('_error').page(`${config.appBaseURL}/random-path`);
+fixture('404_error').page(`${config.appBaseURL}/random-path`);
 
-test('Should navigate to / and then to /login on 404 page', async (t) => {
+test('Should end up on login page after 404', async (t) => {
   await t.expect(getLocation()).eql(`${config.appBaseURL}/login?dest=%2F`);
 });
