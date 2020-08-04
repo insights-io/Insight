85919491ce11beb00bcd0a4443e0ed9d237a36e7
Matej Snuderl <ematej.snuderl@gmail.com>
2020-08-04 12:04:40 +0200

    some e2e tests for sidebar

--- frontend/app/e2e/specs/_app.e2e.ts
+++ frontend/app/e2e/specs/_app.e2e.ts
@@ -1,17 +1,35 @@
-import { Selector } from 'testcafe';
 import { queryByText } from '@testing-library/testcafe';
 
+import { Sidebar } from '../pages';
 import config from '../config';
-import { loginWithInsightUser } from '../utils';
+import { loginWithInsightUser, getLocation } from '../utils';
 
 fixture('_app').page(config.appBaseURL);
 
-test('Should be able to logout', async (t) => {
+test('Should be able to navigate', async (t) => {
   await loginWithInsightUser(t);
 
   await t
-    .hover(Selector('svg[id="account-settings"]'))
+    .hover(Sidebar.accountSettingsItem)
     .click(queryByText('Sign out'))
     .expect(queryByText('Create a free account').visible)
     .ok('Should be on the login page');
+
+  await loginWithInsightUser(t);
+
+  await t
+    .hover(Sidebar.toggleItem)
+    .expect(queryByText('Expand').visible)
+    .ok('Expand sidebar text should exist')
+    .click(Sidebar.toggleItem)
+    .expect(queryByText('Collapse').visible)
+    .ok('Should display text of the toggle sidebar item')
+    .click(Sidebar.sessionsItem)
+    .expect(queryByText('Collapse').visible)
+    .notOk('Collapse text should not be vissible due to sidebar collapsing')
+    .expect(getLocation())
+    .eql(`${config.appBaseURL}/sessions`)
+    .click(Sidebar.homeItem)
+    .expect(getLocation())
+    .eql(config.appBaseURL);
 });
