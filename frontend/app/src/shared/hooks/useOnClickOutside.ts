27f6d8c1cffac1789c296f2cf700f98482d569c0
Matej Snuderl <ematej.snuderl@gmail.com>
2020-08-04 10:04:07 +0200

    fix: navbar collapsing

--- frontend/app/src/shared/hooks/useOnClickOutside.ts
+++ frontend/app/src/shared/hooks/useOnClickOutside.ts
@@ -1,25 +1,59 @@
 import { useEffect, useRef } from 'react';
 
+const findEnclosingSvg = (node: HTMLElement): HTMLElement | null => {
+  let next: Node | null = node.parentNode;
+  while (next?.nodeName !== 'svg') {
+    next = next?.parentNode ?? null;
+  }
+  return next as HTMLElement;
+};
+
 const useOnClickOutside = <T extends HTMLElement>(
-  handler: (ev: MouseEvent | TouchEvent) => void
+  handler: (ev: MouseEvent) => void
 ) => {
   const ref = useRef() as React.RefObject<T>;
 
   useEffect(() => {
-    const listener = (event: MouseEvent | TouchEvent) => {
-      if (!ref.current || ref.current.contains(event.target as Node | null)) {
+    const listener = (event: MouseEvent) => {
+      const target = event.target as HTMLElement;
+      if (!ref.current) {
         return;
+      }
+
+      const { current } = ref;
+      if (current.contains(target)) {
+        return;
+      }
+
+      let svgTarget: HTMLElement | null = target;
+      if (
+        [
+          'path',
+          'rect',
+          'circle',
+          'ellipse',
+          'line',
+          'polyline',
+          'polygon',
+        ].includes(target.nodeName)
+      ) {
+        svgTarget = findEnclosingSvg(target);
+      }
+
+      if (svgTarget && svgTarget.nodeName === 'svg') {
+        const id = target.getAttribute('id');
+        if (current.querySelector(`svg[id="${id}"]`)) {
+          return;
+        }
       }
 
       handler(event);
     };
 
     document.addEventListener('click', listener);
-    document.addEventListener('touchend', listener);
 
     return () => {
       document.removeEventListener('click', listener);
-      document.removeEventListener('touchend', listener);
     };
   }, [ref, handler]);
 
