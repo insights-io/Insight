111288250b64a6da71bdc8e34ec84d8a4bbdd67e
Matej Šnuderl <ematej.snuderl@gmail.com>
2020-08-13 13:47:12 +0200

    storybook composition (#399)
    * storybook composition
    
    * fixes
    
    * fix refs

---
+++ frontend/shared/storybook/.storybook/main.js
@@ -0,0 +1,12 @@
+module.exports = {
+  refs: {
+    app: {
+      title: 'App',
+      url: 'https://insights-io.github.io/app/master',
+    },
+    try: {
+      title: 'Try',
+      url: 'https://insights-io.github.io/try/master',
+    },
+  },
+};
