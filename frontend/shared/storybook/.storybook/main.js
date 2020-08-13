cabd855fd3e3ae9232b2a6927f014aa7b03c1840
Matej Snuderl <ematej.snuderl@gmail.com>
2020-08-13 12:53:15 +0200

    storybook composition

---
+++ frontend/shared/storybook/.storybook/main.js
@@ -0,0 +1,12 @@
+module.exports = {
+  refs: {
+    app: {
+      title: 'App',
+      url: 'https://insights-io.github.io/frontend/app/master',
+    },
+    try: {
+      title: 'Try',
+      url: 'https://insights-io.github.io/frontend/try/master',
+    },
+  },
+};
