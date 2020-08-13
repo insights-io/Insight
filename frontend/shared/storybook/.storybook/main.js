148d57b8e95dbe64b4aa48cc8229023d00483314
Matej Snuderl <ematej.snuderl@gmail.com>
2020-08-13 13:17:56 +0200

    fix refs

--- frontend/shared/storybook/.storybook/main.js
+++ frontend/shared/storybook/.storybook/main.js
@@ -2,11 +2,11 @@ module.exports = {
   refs: {
     app: {
       title: 'App',
-      url: 'https://insights-io.github.io/frontend/app/master',
+      url: 'https://insights-io.github.io/app/master',
     },
     try: {
       title: 'Try',
-      url: 'https://insights-io.github.io/frontend/try/master',
+      url: 'https://insights-io.github.io/try/master',
     },
   },
 };
