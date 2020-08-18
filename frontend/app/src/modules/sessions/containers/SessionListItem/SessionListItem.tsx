35a60f58dc2342457589fd874d6244a6418e5e99
Matej Snuderl <ematej.snuderl@gmail.com>
2020-08-18 11:00:54 +0200

    reuse

--- frontend/app/src/modules/sessions/containers/SessionListItem/SessionListItem.tsx
+++ frontend/app/src/modules/sessions/containers/SessionListItem/SessionListItem.tsx
@@ -1,35 +1,19 @@
 import React from 'react';
-import { Session, UserAgentDTO } from '@insight/types';
-import { ListItem, ARTWORK_SIZES, ListItemLabel } from 'baseui/list';
-import { Tag } from 'baseui/tag';
-import UserAgent from 'shared/components/UserAgent';
-import { FaDesktop, FaMobileAlt } from 'react-icons/fa';
+import { Session } from '@insight/types';
 import { useStyletron } from 'baseui';
-import { formatDistanceToNow } from 'date-fns';
-import { readableLocation } from 'shared/utils/location';
-import { ChevronRight } from 'baseui/icon';
-import { IconType } from 'react-icons/lib';
 import { ListChildComponentProps } from 'react-window';
-import Link from 'next/link';
 import SesssionListItemSkeleton from 'modules/sessions/components/SessionListItemSkeleton';
+import SessionListItem from 'modules/sessions/components/SessionListItem';
 
 type Props = ListChildComponentProps;
 
-const USER_AGENT_DEVICE_ICON_LOOKUP: Record<
-  UserAgentDTO['browserName'],
-  IconType
-> = {
-  Desktop: FaDesktop,
-  Phone: FaMobileAlt,
-};
-
-const SessionListItem = ({ data, index, style }: Props) => {
-  const [css, theme] = useStyletron();
+const SessionListItemContainer = ({ data, index, style }: Props) => {
+  const [_css, theme] = useStyletron();
   const sessions = data as Session[];
 
   const listItemStyle = {
-    borderRadius: theme.sizing.scale100,
     ...style,
+    borderRadius: theme.sizing.scale100,
     top: `${style.top}px`,
   };
 
@@ -37,57 +21,7 @@ const SessionListItem = ({ data, index, style }: Props) => {
     return <SesssionListItemSkeleton style={listItemStyle} />;
   }
 
-  const { id, createdAt, location, userAgent } = sessions[index];
-
-  const createdAtText = formatDistanceToNow(createdAt, {
-    includeSeconds: true,
-    addSuffix: true,
-  });
-
-  const description = [
-    readableLocation(location),
-    location.ip,
-    createdAtText,
-  ].join(' - ');
-
-  const artwork =
-    USER_AGENT_DEVICE_ICON_LOOKUP[userAgent.deviceClass] || FaDesktop;
-
-  return (
-    <Link href="/sessions/[id]" as={`sessions/${id}`} key={id}>
-      <a className={css({ color: 'inherit' })}>
-        <ListItem
-          artwork={artwork}
-          artworkSize={ARTWORK_SIZES.SMALL}
-          overrides={{
-            Root: {
-              style: {
-                ...listItemStyle,
-                ':hover': { background: theme.colors.primary200 },
-              },
-            },
-          }}
-          endEnhancer={() => (
-            <>
-              <Tag
-                closeable={false}
-                overrides={{
-                  Root: { style: { ':hover': { cursor: 'pointer' } } },
-                }}
-              >
-                Details
-              </Tag>
-              <ChevronRight />
-            </>
-          )}
-        >
-          <ListItemLabel description={description}>
-            <UserAgent value={userAgent} />
-          </ListItemLabel>
-        </ListItem>
-      </a>
-    </Link>
-  );
+  return <SessionListItem session={sessions[index]} style={style} />;
 };
 
-export default React.memo(SessionListItem);
+export default React.memo(SessionListItemContainer);
