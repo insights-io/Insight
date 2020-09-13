4cc4df7b6d6d120f7aa7162e4dc427fa83ed764d
Matej Snuderl <ematej.snuderl@gmail.com>
2020-09-13 10:05:25 +0200

    unit test

---
+++ frontend/app/src/modules/auth/pages/LoginPage/EmailForm/LoginEmailForm.spec.tsx
@@ -0,0 +1,36 @@
+import { sandbox } from '@insight/testing';
+import { waitFor } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { AuthApi } from 'api';
+import React from 'react';
+import { render } from 'test/utils';
+
+import LoginEmailForm from './LoginEmailForm';
+
+describe('<LoginEmailForm />', () => {
+  it('Should redirect to verification on negative data response', async () => {
+    const loginStub = sandbox
+      .stub(AuthApi.sso.session, 'login')
+      .resolves({ data: false });
+
+    const replaceStub = sandbox.stub();
+    const { getByPlaceholderText, getByText } = render(
+      <LoginEmailForm replace={replaceStub} relativeRedirect="/" />
+    );
+
+    const email = 'user@gmail.com';
+    const password = 'superPassword';
+    await userEvent.type(getByPlaceholderText('Email'), email);
+    await userEvent.type(getByPlaceholderText('Password'), password);
+
+    userEvent.click(getByText('Sign in'));
+
+    await waitFor(() => {
+      sandbox.assert.calledWithExactly(loginStub, email, password);
+      sandbox.assert.calledWithExactly(
+        replaceStub,
+        '/login/verification?redirect=%2F'
+      );
+    });
+  });
+});
