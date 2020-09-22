b29bf65610642390e228dd7727d959a8820eb246
Matej Snuderl <ematej.snuderl@gmail.com>
2020-09-22 15:51:41 +0200

    fixes

--- infrastructure/terraform/staging/dns/providers.tf
+++ infrastructure/terraform/staging/dns/providers.tf
@@ -1,9 +1,17 @@
+terraform {
+  required_providers {
+    cloudflare = {
+      source  = "cloudflare/cloudflare"
+      version = "2.11.0"
+    }
+  }
+}
+
 provider "aws" {
   version = "3.7.0"
   region  = module.project_vars.region
 }
 
 provider "cloudflare" {
-  version = "2.11.0"
-  email   = "blaz.snuderl@gmail.com"
+  email = "blaz.snuderl@gmail.com"
 }
