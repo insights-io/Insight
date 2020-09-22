7c093285efff20224ec16e02378904330ea5d3e6
Matej Snuderl <ematej.snuderl@gmail.com>
2020-09-22 16:06:03 +0200

    test

--- infrastructure/terraform/modules/browser/versions.tf
+++ infrastructure/terraform/modules/browser/versions.tf
@@ -1,10 +1,16 @@
 terraform {
   required_providers {
+    acme = {
+      source = "terraform-providers/acme"
+    }
     aws = {
       source = "hashicorp/aws"
     }
     cloudflare = {
       source = "cloudflare/cloudflare"
+    }
+    tls = {
+      source = "hashicorp/tls"
     }
   }
   required_version = ">= 0.13"
