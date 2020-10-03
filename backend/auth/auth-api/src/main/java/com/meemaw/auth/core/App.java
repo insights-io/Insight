97b0a8871fb19291ccf689d28287bce93cabf628
Matej Snuderl <ematej.snuderl@gmail.com>
2020-10-03 11:11:47 +0200

    test

--- backend/auth/auth-api/src/main/java/com/meemaw/auth/core/App.java
+++ backend/auth/auth-api/src/main/java/com/meemaw/auth/core/App.java
@@ -6,6 +6,10 @@ import org.eclipse.microprofile.openapi.annotations.info.Info;
 import org.eclipse.microprofile.openapi.annotations.servers.Server;
 
 @OpenAPIDefinition(
-    info = @Info(title = "Auth API", version = "1.0.0"),
+    info = @Info(title = App.TITLE, version = App.VERSION),
     servers = @Server(url = "http://localhost:8080"))
-public class App extends Application {}
+public class App extends Application {
+
+  public static final String TITLE = "Auth API";
+  public static final String VERSION = "1.0.0";
+}
