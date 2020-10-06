6a81c75d08007251328204973a98c2b6c6d8d70e
Matej Snuderl <ematej.snuderl@gmail.com>
2020-10-06 21:58:49 +0200

    chore: require signed commits

--- infrastructure/terraform/github/versions.tf
+++ infrastructure/terraform/github/versions.tf
@@ -1,7 +1,8 @@
 terraform {
   required_providers {
     github = {
-      source = "hashicorp/github"
+      source  = "hashicorp/github"
+      version = "3.0.0"
     }
   }
   required_version = ">= 0.13"
--- infrastructure/terraform/modules/branch_protection/main.tf
+++ infrastructure/terraform/modules/branch_protection/main.tf
@@ -1,8 +1,8 @@
 resource "github_branch_protection" "branch_protection" {
-  repository     = var.repository
-  branch         = var.branch
-  enforce_admins = true
-
+  repository             = var.repository
+  branch                 = var.branch
+  enforce_admins         = true
+  require_signed_commits = true
 
   required_status_checks {
     strict = true
