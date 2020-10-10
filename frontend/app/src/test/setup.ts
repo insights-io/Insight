1edbb62ebb8b4050dafca1fe8ff700088060a081
Matej Šnuderl <ematej.snuderl@gmail.com>
2020-10-10 12:01:32 +0200

    work 2

--- frontend/app/src/test/setup.ts
+++ frontend/app/src/test/setup.ts
@@ -1,4 +1,7 @@
 import { setupEnvironment } from '@insight/testing';
+import { config } from 'dotenv';
 import '@testing-library/jest-dom/extend-expect';
 
 setupEnvironment();
+
+config({ path: '.env.development' });
