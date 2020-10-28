47f086d53f8f066d4f2485f12a71e7ac5b2fa122
Matej Šnuderl <ematej.snuderl@gmail.com>
2020-10-28 12:56:31 +0100

    nprogress

--- frontend/app/src/pages/_app.tsx
+++ frontend/app/src/pages/_app.tsx
@@ -2,8 +2,15 @@ import React from 'react';
 import App from 'next/app';
 import { AppProviders } from 'shared/containers/AppProviders';
 import { createQueryCache } from 'shared/utils/cache';
+import Router from 'next/router';
+import NProgress from 'nprogress';
+import 'nprogress/nprogress.css';
 
 const queryCache = createQueryCache();
+
+Router.events.on('routeChangeStart', () => NProgress.start());
+Router.events.on('routeChangeComplete', () => NProgress.done());
+Router.events.on('routeChangeError', () => NProgress.done());
 
 class InsightApp extends App {
   render() {
