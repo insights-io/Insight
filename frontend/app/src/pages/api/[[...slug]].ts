66c7fedd5f45a491b5a2079587bf43480dc3566c
Matej Šnuderl <ematej.snuderl@gmail.com>
2020-10-31 09:03:35 +0100

    feat: proper proxy

--- frontend/app/src/pages/api/[[...slug]].ts
+++ frontend/app/src/pages/api/[[...slug]].ts
@@ -1,9 +1,12 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import { nextProxy } from '@insight/service-proxy';
 
-export default (
-  originalRequest: NextApiRequest,
-  originalResponse: NextApiResponse
-) => {
-  return nextProxy(originalRequest, originalResponse);
+export default (req: NextApiRequest, res: NextApiResponse) => {
+  return nextProxy(req, res);
+};
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
 };
