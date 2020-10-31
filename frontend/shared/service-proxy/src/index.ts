2255892d6e3e39d196f14b2df900e17bc64d65e5
Matej Šnuderl <ematej.snuderl@gmail.com>
2020-10-31 18:35:12 +0100

    fic 2

--- frontend/shared/service-proxy/src/index.ts
+++ frontend/shared/service-proxy/src/index.ts
@@ -40,7 +40,6 @@ export const nextProxy = (req: NextApiRequest, res: NextApiResponse) => {
       ignorePath: true,
       secure: false,
       changeOrigin: true,
-      xfwd: false,
       cookieDomainRewrite: originalHost.includes('localhost')
         ? ''
         : originalHost,
