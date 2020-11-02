c64dad8f6624fcf0963046d28d6cf2e24b3f9521
Matej Šnuderl <ematej.snuderl@gmail.com>
2020-11-02 15:24:40 +0100

    chore: try-previews (#826)

--- infrastructure/terraform/internal/previews/dns.tf
+++ infrastructure/terraform/internal/previews/dns.tf
@@ -25,3 +25,15 @@ resource "github_actions_secret" "dev_domain" {
   secret_name     = "DEV_DOMAIN_NAME"
   plaintext_value = module.project_vars.domain
 }
+
+module "try_previews_wildcard_certificate" {
+  source  = "../../modules/certificate"
+  domain  = "*.try.${module.project_vars.domain}"
+  zone_id = data.aws_route53_zone.zone.zone_id
+}
+
+resource "github_actions_secret" "try_previews_wildcard_certificate_arn" {
+  repository      = module.global_vars.monorepo_repository
+  secret_name     = "AWS_TRY_PREVIEW_CERTIFICATE_ARN"
+  plaintext_value = module.try_previews_wildcard_certificate.arn
+}
--- infrastructure/terraform/internal/previews/storage.tf
+++ infrastructure/terraform/internal/previews/storage.tf
@@ -1,10 +1,10 @@
-resource "aws_s3_bucket" "app_previews" {
-  bucket = "app-previews"
+resource "aws_s3_bucket" "frontend_previews" {
+  bucket = "frontend-previews"
   acl    = "private"
 }
 
-resource "github_actions_secret" "app_previews" {
+resource "github_actions_secret" "frontend_previews" {
   repository      = module.global_vars.monorepo_repository
-  secret_name     = "AWS_S3_APP_PREVIEW_BUCKET_NAME"
-  plaintext_value = aws_s3_bucket.app_previews.id
+  secret_name     = "AWS_S3_FRONTEND_PREVIEWS_BUCKET_NAME"
+  plaintext_value = aws_s3_bucket.frontend_previews.id
 }
