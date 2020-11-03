f907dba293ad05ad09c371f229486a85dbcd497b
Matej Šnuderl <ematej.snuderl@gmail.com>
2020-11-03 10:07:05 +0100

    chore: codeql scanning (#832)

--- backend/shared/rest-api/src/main/java/com/meemaw/shared/config/ConfigureApplication.java
+++ backend/shared/rest-api/src/main/java/com/meemaw/shared/config/ConfigureApplication.java
@@ -1,20 +1,13 @@
 package com.meemaw.shared.config;
 
 import com.rebrowse.Rebrowse;
-import com.rebrowse.net.ApiResource;
-import com.rebrowse.net.NetHttpClient;
-import com.rebrowse.net.RebrowseHttpClient;
 import io.quarkus.runtime.StartupEvent;
 import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.event.Observes;
-import javax.inject.Inject;
 import org.eclipse.microprofile.config.inject.ConfigProperty;
-import org.eclipse.microprofile.context.ManagedExecutor;
 
 @ApplicationScoped
 public class ConfigureApplication {
-
-  @Inject ManagedExecutor managedExecutor;
 
   @ConfigProperty(name = "authorization.s2s.api.key")
   String s2sApiKey;
@@ -24,14 +17,7 @@ public class ConfigureApplication {
   }
 
   private void configureHttpClient() {
-    java.net.http.HttpClient.Builder builder = java.net.http.HttpClient.newBuilder();
-    if (managedExecutor != null) {
-      builder.executor(managedExecutor);
-    }
-
     Rebrowse.apiKey = s2sApiKey;
     Rebrowse.maxNetworkRetries(2);
-    ApiResource.setHttpClient(
-        new RebrowseHttpClient(new NetHttpClient(builder.build()), ApiResource.OBJECT_MAPPER));
   }
 }
