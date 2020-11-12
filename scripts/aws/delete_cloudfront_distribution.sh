114e4c59b2e8dd0293ecf1829e99213e47efafe9
Matej Šnuderl <ematej.snuderl@gmail.com>
2020-11-12 20:42:25 +0100

    quotes

--- scripts/aws/delete_cloudfront_distribution.sh
+++ scripts/aws/delete_cloudfront_distribution.sh
@@ -8,6 +8,6 @@ if [ -z "$DISTRIBUTION_ID" ]
     exit 1
 fi
 
-aws cloudfront wait distribution-deployed --id $DISTRIBUTION_ID
-ETAG=$(aws cloudfront get-distribution-config --id $DISTRIBUTION_ID | jq -r '.ETag')
-aws cloudfront delete-distribution --id $DISTRIBUTION_ID --if-match $ETAG
+aws cloudfront wait distribution-deployed --id "$DISTRIBUTION_ID"
+ETAG=$(aws cloudfront get-distribution-config --id "$DISTRIBUTION_ID" | jq -r '.ETag')
+aws cloudfront delete-distribution --id "$DISTRIBUTION_ID" --if-match "$ETAG"
