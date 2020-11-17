0649324042dda9b72e2030a4517c4f7f2f7631cd
Matej Šnuderl <ematej.snuderl@gmail.com>
2020-11-17 09:37:10 +0100

    fix(ci): new path envs (#881)

--- frontend/shared/elements/.storybook/main.js
+++ frontend/shared/elements/.storybook/main.js
@@ -1,4 +1,8 @@
 module.exports = {
   stories: ['../src/**/*.stories.tsx'],
-  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+  addons: [
+    '@storybook/addon-essentials',
+    '@storybook/addon-links',
+    '@storybook/addon-storysource',
+  ],
 };
