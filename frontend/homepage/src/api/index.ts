35c5f051019a21ed17d70e58411eb9eb8edd287e
Matej Šnuderl <ematej.snuderl@gmail.com>
2020-11-20 17:14:19 +0100

    fix typo

--- frontend/homepage/src/api/index.ts
+++ frontend/homepage/src/api/index.ts
@@ -1,4 +1,4 @@
-import { ssoSessionResource } from '@insirebrowseght/sdk';
+import { ssoSessionResource } from '@rebrowse/sdk';
 import { AUTH_API_BASE_URL } from 'shared/constants';
 
 export default ssoSessionResource(AUTH_API_BASE_URL);
