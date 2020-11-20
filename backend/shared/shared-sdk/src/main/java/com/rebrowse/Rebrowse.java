010606f063b57c3e35b5f6afdfd4803c648e0ba7
Matej Šnuderl <ematej.snuderl@gmail.com>
2020-11-20 14:42:37 +0100

    fail

--- backend/shared/shared-sdk/src/main/java/com/rebrowse/Rebrowse.java
+++ backend/shared/shared-sdk/src/main/java/com/rebrowse/Rebrowse.java
@@ -4,7 +4,7 @@ import java.time.Duration;
 
 public final class Rebrowse {
 
-  public static final Duration TIMEOUT = Duration.ofSeconds(30);
+  public static final Duration TIMEOUT = Duration.ofSeconds(10);
 
   public static final String API_BASE = "https://api.rebrowse.io";
   public static final String VERSION = "1.0.0";
