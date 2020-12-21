fd0ebbc859b7264dec5596b2421505c209ba6bd0
Matej Šnuderl <ematej.snuderl@gmail.com>
2020-12-21 16:34:45 +0100

    fix some tests

--- frontend/app/src/test/mocks/filter/core.spec.ts
+++ frontend/app/src/test/mocks/filter/core.spec.ts
@@ -1,84 +1,85 @@
 import { SearchBean, SessionDTO } from '@rebrowse/types';
 import { addHours, subHours } from 'date-fns';
-import { REBROWSE_SESSIONS_DTOS } from 'test/data';
+import { REBROWSE_SESSIONS, REBROWSE_SESSIONS_DTOS } from 'test/data';
 
 import { filterByParam } from './core';
 
 describe('filter', () => {
   describe('filterByParam', () => {
     it('date', () => {
-      const session = REBROWSE_SESSIONS_DTOS[0];
+      const sessionDto = REBROWSE_SESSIONS_DTOS[0];
+      const session = REBROWSE_SESSIONS[0];
 
       const filterByCreatedAt = (search: SearchBean<SessionDTO>) =>
-        filterByParam(session, 'createdAt', search, (v: string) =>
+        filterByParam(sessionDto, 'createdAt', search, (v: string) =>
           new Date(v).valueOf()
         );
 
       /* gt */
       expect(
-        filterByCreatedAt({ createdAt: `gt:${session.createdAt}` })
+        filterByCreatedAt({ createdAt: `gt:${sessionDto.createdAt}` })
       ).toBeFalsy();
 
       expect(
         filterByCreatedAt({
-          createdAt: `gt:${subHours(new Date(), 1).toISOString()}`,
+          createdAt: `gt:${subHours(session.createdAt, 1).toISOString()}`,
         })
       ).toBeTruthy();
 
       expect(
         filterByCreatedAt({
-          createdAt: `gt:${addHours(new Date(), 1).toISOString()}`,
+          createdAt: `gt:${addHours(session.createdAt, 1).toISOString()}`,
         })
       ).toBeFalsy();
 
       /* gte */
       expect(
-        filterByCreatedAt({ createdAt: `gte:${session.createdAt}` })
+        filterByCreatedAt({ createdAt: `gte:${sessionDto.createdAt}` })
       ).toBeTruthy();
 
       expect(
         filterByCreatedAt({
-          createdAt: `gte:${subHours(new Date(), 1).toISOString()}`,
+          createdAt: `gte:${subHours(session.createdAt, 1).toISOString()}`,
         })
       ).toBeTruthy();
 
       expect(
         filterByCreatedAt({
-          createdAt: `gte:${addHours(new Date(), 1).toISOString()}`,
+          createdAt: `gte:${addHours(session.createdAt, 1).toISOString()}`,
         })
       ).toBeFalsy();
 
       /* lt */
       expect(
-        filterByCreatedAt({ createdAt: `lt:${session.createdAt}` })
+        filterByCreatedAt({ createdAt: `lt:${sessionDto.createdAt}` })
       ).toBeFalsy();
 
       expect(
         filterByCreatedAt({
-          createdAt: `lt:${subHours(new Date(), 1).toISOString()}`,
+          createdAt: `lt:${subHours(session.createdAt, 1).toISOString()}`,
         })
       ).toBeFalsy();
 
       expect(
         filterByCreatedAt({
-          createdAt: `lt:${addHours(new Date(), 1).toISOString()}`,
+          createdAt: `lt:${addHours(session.createdAt, 1).toISOString()}`,
         })
       ).toBeTruthy();
 
       /* lte */
       expect(
-        filterByCreatedAt({ createdAt: `lte:${session.createdAt}` })
+        filterByCreatedAt({ createdAt: `lte:${sessionDto.createdAt}` })
       ).toBeTruthy();
 
       expect(
         filterByCreatedAt({
-          createdAt: `lte:${subHours(new Date(), 1).toISOString()}`,
+          createdAt: `lte:${subHours(session.createdAt, 1).toISOString()}`,
         })
       ).toBeFalsy();
 
       expect(
         filterByCreatedAt({
-          createdAt: `lte:${addHours(new Date(), 1).toISOString()}`,
+          createdAt: `lte:${addHours(session.createdAt, 1).toISOString()}`,
         })
       ).toBeTruthy();
     });
