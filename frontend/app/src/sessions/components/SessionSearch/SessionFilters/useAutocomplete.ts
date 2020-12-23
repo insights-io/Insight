1db3e8d846e6b87af11138f7c3b5610ea96731e4
Matej Šnuderl <ematej.snuderl@gmail.com>
2020-12-23 20:34:42 +0100

    fix compile error

--- frontend/app/src/sessions/components/SessionSearch/SessionFilters/useAutocomplete.ts
+++ frontend/app/src/sessions/components/SessionSearch/SessionFilters/useAutocomplete.ts
@@ -1,15 +1,18 @@
+import { SessionSearchQueryParams } from '@rebrowse/sdk';
 import { SessionApi } from 'api';
 import { useQuery } from 'shared/hooks/useQuery';
 
-export const cacheKey = (on: string | undefined) => {
+export const cacheKey = (on: keyof SessionSearchQueryParams | undefined) => {
   return ['SessionApi', 'distinct', on];
 };
 
-const queryFn = (on: string | undefined) => {
+const queryFn = (on: keyof SessionSearchQueryParams | undefined) => {
   return on === undefined ? [] : SessionApi.distinct(on);
 };
 
-export const useAutocomplete = (on: string | undefined) => {
+export const useAutocomplete = (
+  on: keyof SessionSearchQueryParams | undefined
+) => {
   const { data: options = [] } = useQuery(cacheKey(on), () => queryFn(on));
   return { options };
 };
