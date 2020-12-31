4b4b6d275d22e66d8f0d0c17eaac38dd6a2152f8
Matej Šnuderl <ematej.snuderl@gmail.com>
2020-12-31 17:56:06 +0100

    chore(deps): bump next (#1084)

--- frontend/app/src/__tests__/pages/settings/organization/subscription/id.spec.ts
+++ frontend/app/src/__tests__/pages/settings/organization/subscription/id.spec.ts
@@ -1,17 +1,78 @@
+import { mockApiError } from '@rebrowse/storybook';
 import { sandbox } from '@rebrowse/testing';
 import { screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
+import { BillingApi } from 'api';
 import { getPage } from 'next-page-tester';
 import { ORGANIZATION_SETTINGS_BILLING_SUBSCRIPTION_PAGE } from 'shared/constants/routes';
 import { match } from 'sinon';
 import { ACTIVE_BUSINESS_SUBSCRIPTION_DTO } from '__tests__/data/billing';
-import { mockOrganizationSettingsSubscriptionDetailsPage } from '__tests__/mocks';
+import {
+  mockOrganizationSettingsSubscriptionDetailsPage,
+  mockOrganizationSettingsSubscriptionPage,
+} from '__tests__/mocks';
 import { renderPage } from '__tests__/utils';
 
 describe('/settings/organization/subscription/[id]', () => {
   /* Data */
   const subscription = ACTIVE_BUSINESS_SUBSCRIPTION_DTO;
   const route = `${ORGANIZATION_SETTINGS_BILLING_SUBSCRIPTION_PAGE}/${subscription.id}`;
+
+  test('As a user I should be redirected to /settings/organization/subscription on 404 request', async () => {
+    /* Mocks */
+    document.cookie = 'SessionId=123';
+    const {
+      listSubscriptionsStub,
+      retrieveActivePlanStub,
+    } = mockOrganizationSettingsSubscriptionPage(sandbox);
+
+    const retrieveSubscriptionStub = sandbox
+      .stub(BillingApi.subscriptions, 'get')
+      .rejects(
+        mockApiError({
+          statusCode: 404,
+          reason: 'Not Found',
+          message: 'Not Found',
+        })
+      );
+
+    /* Server */
+    const { page } = await getPage({ route });
+
+    sandbox.assert.calledOnceWithExactly(
+      retrieveSubscriptionStub,
+      subscription.id,
+      {
+        baseURL: 'http://localhost:8083',
+        headers: {
+          cookie: 'SessionId=123',
+          'uber-trace-id': (match.string as unknown) as string,
+        },
+      }
+    );
+
+    sandbox.assert.calledWithExactly(listSubscriptionsStub, {
+      baseURL: 'http://localhost:8083',
+      headers: {
+        cookie: 'SessionId=123',
+        'uber-trace-id': (match.string as unknown) as string,
+      },
+      search: { sortBy: ['-createdAt'] },
+    });
+    sandbox.assert.calledWithExactly(retrieveActivePlanStub, {
+      baseURL: 'http://localhost:8083',
+      headers: {
+        cookie: 'SessionId=123',
+        'uber-trace-id': (match.string as unknown) as string,
+      },
+    });
+
+    /* Client */
+    renderPage(page);
+
+    expect(screen.getByText('Rebrowse Enterprise')).toBeInTheDocument();
+    expect(screen.getByText('0 of ∞ sessions')).toBeInTheDocument();
+  });
 
   test('As a user I should see details about active subscription & terminate it', async () => {
     /* Mocks */
